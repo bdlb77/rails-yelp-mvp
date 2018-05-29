1d79bdece7bb955f33b944fc9c7eb538a6f90307
Bryan Leighton <bryan.leigh77@gmail.com>
2018-05-29 12:55:59 +0300

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:      'Dishoom',
+    address:   '7 Boundary St, London E2 7JE',
+    category:  "chinese"
+  },
+  {
+    name:      'Pizza East',
+    address:   '56A Shoreditch High St, London E1 6PQ',
+    category:  "italian"
+  },
+  {
+    name:      "Jus D'Orange",
+    address:   '56A Paris Street, Paris',
+    category:  "french"
+  },
+  {
+    name:         'Kompai',
+    address:      '5544 Osaka, Japan',
+    category:  "japanese"
+  },
+  {
+    name:         'Moules Frite',
+    address:      'Brussels, Belgium',
+    category:  	"belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
